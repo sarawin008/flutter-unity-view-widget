8e2e3c1d71586e7928aa09f84f7960043e091709
k._lled <saberhusky474@gmail.com>
2020-03-31 17:33:56 +0700

    Update FlutterUnityView.java

--- android/src/main/java/com/rexraphael/flutterunitywidget/FlutterUnityView.java
+++ android/src/main/java/com/rexraphael/flutterunitywidget/FlutterUnityView.java
@@ -89,7 +89,7 @@ public class FlutterUnityView implements PlatformView, MethodChannel.MethodCallH
     @Override
     public void dispose() {
         if (UnityUtils.isUnityReady()) {
-            UnityUtils.getPlayer().quit();
+            //UnityUtils.getPlayer().quit();
         }
     }
 
